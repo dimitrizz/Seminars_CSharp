b432bf6d1d5892bb8dcf001c0e38c0b546a08559
Dimitriizz <dmitriiqtr@gmail.com>
2022-11-26 19:10:48 +0400

    add Example 2

---
+++ Example_2/Program.cs
@@ -0,0 +1,25 @@
+﻿
+//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+int namberA = int.Parse(Console.ReadLine());
+int namberB = int.Parse(Console.ReadLine());
+int namberC = int.Parse(Console.ReadLine());
+
+int max = 0;
+
+
+if (namberA > namberB) {
+    max = namberA;
+} else {
+    max = namberB;
+}
+
+if (max > namberC) {
+    Console.WriteLine(max);
+} else {
+    max = namberC;
+    Console.WriteLine(max);
+}
+
+
+
