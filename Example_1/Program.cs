24679cfcbb0117743d9cbc51a02dd25a01a83a84
Dimitriizz <dmitriiqtr@gmail.com>
2022-11-26 18:44:35 +0400

    add Example 1

--- Example_1/Program.cs
+++ Example_1/Program.cs
@@ -1,1 +1,11 @@
 ﻿
+//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+int namberA = int.Parse(Console.ReadLine());
+int namberB = int.Parse(Console.ReadLine());
+
+if (namberA > namberB) {
+    Console.WriteLine("Число " + namberA + " больше, а число " + namberB + " меньше.");
+} else {
+   Console.WriteLine("Число " + namberB + " больше, а число " + namberA + " меньше."); 
+}
